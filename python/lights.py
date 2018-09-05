6c21c8b1091ea7af33757c98b121f31cdb653a8e
Michal Rak <mr1e14@soton.ac.uk>
2018-09-05 18:33:10 +0000

    lights: add fade effect

--- python/lights.py
+++ python/lights.py
@@ -1,5 +1,6 @@
 from yeelight import Bulb, Flow, discover_bulbs
 from yeelight.transitions import *
+import threading
 
 
 def _initialize_lights():
@@ -66,4 +67,56 @@ class LightManager:
     def set_default(self, name):
         self.default = self.get_light_by_name(name)
 
+    def fade(self, duration, turn_off=False, *bulbs):
+        max_requests_per_minute = 40
+        min_interval = 60 / max_requests_per_minute  # 60 seconds
 
+        if len(bulbs) == 0:
+            bulbs = [self.default]
+
+        for bulb in bulbs:
+            initial_brightness = bulb.get_properties(['bright']).get('bright')
+            interval = duration / _get_total_fade_steps(initial_brightness)
+            interval = max(min_interval, interval)
+            current_props = bulb.get_properties(['bright', 'ct', 'rgb', 'flowing'])
+            _fade(bulb, interval, current_props, turn_off)
+
+
+def _fade(bulb, interval, props, turn_off):
+        # if another request was made, abort task
+        if props != bulb.get_properties(['bright', 'ct', 'rgb', 'flowing']):
+            return
+
+        power, brightness = bulb.get_properties(['power', 'bright']).values()
+
+        if power == 'off':
+            return
+        if brightness <= 1:
+            if turn_off:
+                bulb.turn_off()
+            return
+
+        new_brightness = __get_decreased_brightness(brightness)
+        bulb.set_brightness(new_brightness)
+
+        props = bulb.get_properties(['bright', 'ct', 'rgb', 'flowing'])
+        threading.Timer(interval, _fade, [bulb, interval, props, turn_off]).start()
+
+
+def _get_total_fade_steps(initial_brightness):
+    steps = 0
+    brightness = initial_brightness
+
+    while brightness > 1:
+        steps += 1
+        brightness = __get_decreased_brightness(brightness)
+
+    return steps
+
+
+def __get_decreased_brightness(brightness):
+    max_brightness = 1
+    min_brightness = 99
+    diff = max(1, round(brightness - (brightness / 10)))
+
+    return max(min_brightness, min((brightness - diff), max_brightness))
