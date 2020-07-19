844a6ad5136f8496c626023363de482268fbefe2
Michal Rak <mr1e14@soton.ac.uk>
2020-07-19 09:53:10 +0100

    make config path private

--- python/iot_app/config/config.py
+++ python/iot_app/config/config.py
@@ -1,5 +1,5 @@
 import yaml
 import os
 
-config_path = os.path.join(os.getcwd(), 'iot_app', 'config', 'config.yaml')
+_config_path = os.path.join(os.getcwd(), 'iot_app', 'config', 'config.yaml')
 config = yaml.safe_load(open(config_path))
