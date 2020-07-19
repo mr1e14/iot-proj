6ee5caed34a3f60ef435cbdc8ad3d4743d5b6870
Michal Rak <mr1e14@soton.ac.uk>
2020-07-19 10:13:59 +0100

    fix config path reference

--- python/iot_app/config/config.py
+++ python/iot_app/config/config.py
@@ -2,4 +2,4 @@ import yaml
 import os
 
 _config_path = os.path.join(os.getcwd(), 'iot_app', 'config', 'config.yaml')
-config = yaml.safe_load(open(config_path))
+config = yaml.safe_load(open(_config_path))
